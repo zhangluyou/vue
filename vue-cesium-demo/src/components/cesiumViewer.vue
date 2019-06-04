db0ee155426f49afe859c0121710365c3ee8a7b3
zhanglu <2593027052@qq.com>
2019-06-04 16:54:35 +0800

    vue整合cesium

---
+++ vue-cesium-demo/src/components/cesiumViewer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="cesiumContainer">
+  </div>
+</template>
+
+<script>
+import Cesium from 'cesium/Cesium'
+import widgets from 'cesium/Widgets/widgets.css'
+export default {
+  name: 'cesiumContainer',
+  mounted () {
+    let viewer = new Cesium.Viewer('cesiumContainer')
+  }
+
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue-cesium-demo/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import cesiumViewer from '@/components/cesiumViewer'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'cesiumViewer',
+      component: cesiumViewer
+    },
+    {
+      path: '/map',
+      name: 'cesiumViewer',
+      component: cesiumViewer
+    }
+  ]
+})
